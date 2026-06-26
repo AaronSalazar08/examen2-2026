80b2979331104c253f693d8386d37875fab28a30
Dann233 <ds005110@gmail.com>
2026-06-26 14:32:07 -0600

    creacion de migracion de unidad

--- database/migrations/2026_06_26_192005_create_unidads_table.php
+++ database/migrations/2026_06_26_192005_create_unidads_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('unidads', function (Blueprint $table) {
-            $table->id();                  // idUnidad en el dominio
+            $table->id();                
             $table->string('nombre');
             $table->timestamps();
         });
