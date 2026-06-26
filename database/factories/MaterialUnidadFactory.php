f88ec69cddda42e2c6db61f8323f2ae198c19797
AaronSalazar08 <aaron.salazar.mata@gmail.com>
2026-06-26 14:03:09 -0600

    Modelos, migraciones, factories y controllers

---
+++ database/factories/MaterialUnidadFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MaterialUnidad;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<MaterialUnidad>
+ */
+class MaterialUnidadFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/RequisicionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Requisicion;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Requisicion>
+ */
+class RequisicionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
